cdb96b737199efd46741935e80e338d7c98d0823
Kai Bertelsen <kai@exposoft.no>
2025-11-20 13:05:29 +0100

    ny

--- loader.js
+++ loader.js
@@ -1,37 +1,30 @@
+// Loader for å hente inn eksterne skript fra CDN
+function loadScript(url) {
+    return new Promise((resolve, reject) => {
+        const script = document.createElement('script');
+        script.src = url;
+        script.onload = () => resolve();
+        script.onerror = () => reject(`Failed to load script: ${url}`);
+        document.head.appendChild(script);
+    });
+}
 
-/* Denne skal ligge i webflow
-<script src="https://kaibertelsen.github.io/KonsernKontroll/loader.js"></script>
-*/
-
-
-    function loadScript(url) {
-        return new Promise((resolve, reject) => {
-            const script = document.createElement('script');
-            script.src = url;
-            script.onload = () => resolve();
-            script.onerror = () => reject(`Failed to load script: ${url}`);
-            document.head.appendChild(script);
-        });
-    }
-    
-    // Liste over CDN-URL-er som skal lastes inn
-// Liste over CDN-URL-er som skal lastes inn
+// Liste over CDN-scripts
 const cdnScripts = [
     "https://kaibertelsen.github.io/KonsernKontroll/vendor-neonApiClient.js",
     "https://kaibertelsen.github.io/KonsernKontroll/KK.bundle.js",
-    
 ];
 
-
-    
-    // Når alle scripts er ferdiglastet:
-    cdnScripts.reduce((promise, script) => {
-        return promise.then(() => loadScript(script));
-    }, Promise.resolve()).then(() => {
+// Når alle scripts er ferdig lastet:
+cdnScripts.reduce((p, url) => p.then(() => loadScript(url)), Promise.resolve())
+    .then(() => {
         console.log("All scripts loaded");
-        if (typeof startUp === "function") startUp();   // <–– KJØR START!
-    }).catch(error => {
-        console.error(error);
-    });
-    
-    
+
+        // Riktig entrypoint for bundle C1:
+        if (window.KonsernKontroll && typeof KonsernKontroll.startUp === "function") {
+            KonsernKontroll.startUp();
+        } else {
+            console.error("KonsernKontroll.startUp() mangler!");
+        }
+    })
+    .catch(err => console.error(err));
